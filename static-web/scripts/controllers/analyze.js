f326d784fce1b0850c006c8901ddf952d039d9d4
jeffrichley <jeffrichley@gmail.com>
2014-08-05 14:48:45 -0400

    now able to compare more than one item at a time

--- static-web/scripts/controllers/analyze.js
+++ static-web/scripts/controllers/analyze.js
@@ -15,44 +15,61 @@ angular.module('glassApp').controller('AnalyzeCtrl', function ($scope, dataservi
 	dataservice.getUIFields().then(function(data) {
 		$scope.headers = data.headers;
 		$scope.selectedHeaders = [];
+		$scope.selectedKey = null;
 		
 		$scope.buttonStyles = [];
 		for (var i = 0; i < $scope.headers.length; i++) {
-			$scope.buttonStyles[i] = 'btn btn-lg btn-primary btn-block';
+			$scope.buttonStyles[i] = 'btn btn-lg btn-primary nsc-header-btn';
 			$scope.selectedHeaders[i] = false;
 		}
 		
 		$scope.$watchCollection('selectedHeaders', function(newValue, oldValue){
 			for (var i = 0; i < newValue.length; i++) {
 				if (newValue[i]) {
-					$scope.buttonStyles[i] = 'btn btn-lg btn-info ';
+					$scope.buttonStyles[i] = 'btn btn-lg btn-info nsc-header-btn';
+					if ($scope.selectedKey == null && newValue[i] != oldValue[i]) {
+						$scope.selectedKey = i;
+					}
 				} else {
-					$scope.buttonStyles[i] = 'btn btn-lg btn-primary ';
+					$scope.buttonStyles[i] = 'btn btn-lg btn-primary nsc-header-btn';
 				}
 			} 
 		});
 	});
 
 	// setup actions
+	$scope.selectKey = function(index) {
+		if ($scope.selectedKey == index) {
+			$scope.selectedKey = null;
+		} else {
+			$scope.selectedKey = index;
+			$scope.selectedHeaders[index] = true;
+		}
+	};
 	
 	// draw the charts that come back from the server 
 	$scope.compare = function() {
 		$scope.showInitialSplash = false;
 		
-		var fieldOne = null;
-		var fieldTwo = null;
+		var fieldOne = $scope.headers[$scope.selectedKey];
+		var fieldTwos = [];
 		for (var i = 0; i < $scope.selectedHeaders.length; i++) {
-			if ($scope.selectedHeaders[i]) {
-				if (fieldOne) {
-					fieldTwo = $scope.headers[i];
-				} else {
-					fieldOne = $scope.headers[i];
-				}
+			if ($scope.selectedHeaders[i] && i != $scope.selectedKey) {
+				fieldTwos.push($scope.headers[i]);
+//				if (fieldOne) {
+//					fieldTwo = $scope.headers[i];
+//				} else {
+//					fieldOne = $scope.headers[i];
+//				}
 			}
 		}
 		
 		
-		var header = '<div class="navbar navbar-default" role="navigation">' +
+		for (var i = 0; i < fieldTwos.length; i++) {
+			
+			var fieldTwo = fieldTwos[i];
+
+			var header = '<div class="navbar navbar-default" role="navigation">' +
 		  			 '	<div class="container-fluid">' +
 		  			 '		<div class="navbar-header">' +
 		  			 '			<a class="navbar-brand" href="#">Comparing '+fieldOne+' vs. '+fieldTwo+'</a>' +
@@ -60,97 +77,104 @@ angular.module('glassApp').controller('AnalyzeCtrl', function ($scope, dataservi
 		  			 '	</div>' +
 		  			 '</div>';
 		
-		var progressBar = '<div class="progress">' +
+			var progressBar = '<div class="progress">' +
 		  				  '	 <div class="progress-bar progress-bar-striped active"  role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">' +
 		  				  '	   <span class="sr-only"></span>' +
 		  				  '  </div>' +
 		  				  '</div>';
 		
+			
+			
+			
+			$('#charts').prepend(header + '<div id="'+fieldOne+'-'+fieldTwo+'-compare">'+progressBar+'</div><div></div>');
 		
-		$('#charts').prepend(header + '<div id="'+fieldOne+'-'+fieldTwo+'-compare">'+progressBar+'</div><div></div>');
-	
-		dataservice.compare(fieldOne, fieldTwo).then(function(data){
-			// set up the data for the chart
-			if (data.compareType == 'NUMERIC_NUMERIC') {
-				var chartDataValues = [];
-				chartDataValues.push([data.fieldOne, data.fieldTwo]);
-				for (var i = 0; i < data.points.length; i++) {
-					var pair = data.points[i];
-					var entry = [pair.x, pair.y];
-					chartDataValues.push(entry);
-				}
-				var chartData = google.visualization.arrayToDataTable(chartDataValues);
-				var options = {
-						title: data.title,
-						hAxis: {title: data.fieldOne},
-						vAxis: {title: data.fieldTwo},
-						legend: 'none',
-						height: 400
-				};
-				var chart = new google.visualization.ScatterChart(document.getElementById(data.fieldOne+'-'+data.fieldTwo+'-compare'));
-				chart.draw(chartData, options);
-			} else if (data.compareType == 'LABEL_NUMERIC') {
-				var chartData = new google.visualization.DataTable();
-				chartData.addColumn('string', 'Entries');
-				chartData.addColumn('number', 'Sum');
-				chartData.addColumn('number', 'Count');
-				chartData.addColumn('number', 'Average');
-				chartData.addColumn('number', 'Median');
-				
-				var chartDataValues = [];
-				for (var i = 0; i < data.pairs.length; i++) {
-					var pair = data.pairs[i];
-					var entry = [pair.label, pair.sum, pair.count, pair.average, pair.median];
-					chartDataValues.push(entry);
-				}
-				chartData.addRows(chartDataValues);
-				
-				var options = {
-					showRowNumber: true
-				};
-				
-				var div = document.getElementById(data.fieldOne+'-'+data.fieldTwo+'-compare');
-				if (div == null) {
-					div = document.getElementById(data.fieldTwo+'-'+data.fieldOne+'-compare');
-				}
-				
-				// Google table doesn't support title
-//				$(div).prepend('<div>'+data.title+'</div>');
-				
-				var table = new google.visualization.Table(div);
-		        table.draw(chartData, options);
-			} else if (data.compareType == 'LABEL_LABEL') {
-				var chartData = new google.visualization.DataTable();
-				chartData.addColumn('string', data.fieldOne);
-				for (var i = 0; i < data.columnLabels.length; i++) {
-					chartData.addColumn('number', data.columnLabels[i]);
-				}
-				
-				var chartDataValues = [];
-				for (var i = 0; i < data.rowLabels.length; i++) {
-					var entry = [data.rowLabels[i]];
-					for (var j = 0; j < data.columnLabels.length; j++) {
-						entry.push(data.data[i][j]);
+			dataservice.compare(fieldOne, fieldTwo).then(function(data) {
+				// set up the data for the chart
+				if (data.compareType == 'NUMERIC_NUMERIC') {
+					var chartDataValues = [];
+					chartDataValues.push([data.fieldOne, data.fieldTwo]);
+					for (var i = 0; i < data.points.length; i++) {
+						var pair = data.points[i];
+						var entry = [pair.x, pair.y];
+						chartDataValues.push(entry);
 					}
-					chartDataValues.push(entry);
+					var chartData = google.visualization.arrayToDataTable(chartDataValues);
+					var options = {
+							title: data.title,
+							hAxis: {title: data.fieldOne},
+							vAxis: {title: data.fieldTwo},
+							legend: 'none',
+							height: 400
+					};
+					var chart = new google.visualization.ScatterChart(document.getElementById(data.fieldOne+'-'+data.fieldTwo+'-compare'));
+					chart.draw(chartData, options);
+				} else if (data.compareType == 'LABEL_NUMERIC') {
+					var chartData = new google.visualization.DataTable();
+					chartData.addColumn('string', 'Entries');
+					chartData.addColumn('number', 'Sum');
+					chartData.addColumn('number', 'Count');
+					chartData.addColumn('number', 'Average');
+					chartData.addColumn('number', 'Median');
+					
+					var chartDataValues = [];
+					for (var i = 0; i < data.pairs.length; i++) {
+						var pair = data.pairs[i];
+						var entry = [pair.label, pair.sum, pair.count, pair.average, pair.median];
+						chartDataValues.push(entry);
+					}
+					chartData.addRows(chartDataValues);
+					
+					var options = {
+						showRowNumber: true
+					};
+					
+					var div = document.getElementById(data.fieldOne+'-'+data.fieldTwo+'-compare');
+					if (div == null) {
+						div = document.getElementById(data.fieldTwo+'-'+data.fieldOne+'-compare');
+					}
+					
+					// Google table doesn't support title
+	//				$(div).prepend('<div>'+data.title+'</div>');
+					
+					var table = new google.visualization.Table(div);
+			        table.draw(chartData, options);
+				} else if (data.compareType == 'LABEL_LABEL') {
+					var chartData = new google.visualization.DataTable();
+					chartData.addColumn('string', data.fieldOne);
+					for (var i = 0; i < data.columnLabels.length; i++) {
+						chartData.addColumn('number', data.columnLabels[i]);
+					}
+					
+					var chartDataValues = [];
+					for (var i = 0; i < data.rowLabels.length; i++) {
+						var entry = [data.rowLabels[i]];
+						for (var j = 0; j < data.columnLabels.length; j++) {
+							entry.push(data.data[i][j]);
+						}
+						chartDataValues.push(entry);
+					}
+					chartData.addRows(chartDataValues);
+					
+					var div = document.getElementById(data.fieldOne+'-'+data.fieldTwo+'-compare');
+					if (div == null) {
+						div = document.getElementById(data.fieldTwo+'-'+data.fieldOne+'-compare');
+					}
+					
+					var options = {
+						showRowNumber: true
+					};
+					
+					var table = new google.visualization.Table(div);
+			        table.draw(chartData, options);
 				}
-				chartData.addRows(chartDataValues);
-				
-				var div = document.getElementById(data.fieldOne+'-'+data.fieldTwo+'-compare');
-				if (div == null) {
-					div = document.getElementById(data.fieldTwo+'-'+data.fieldOne+'-compare');
-				}
-				
-				var options = {
-					showRowNumber: true
-				};
-				
-				var table = new google.visualization.Table(div);
-		        table.draw(chartData, options);
-			}
-			
-			
 		});
+			
+			
+			
+			
+			
+			
+		}
 	};
 	
 	$scope.describe = function() {
